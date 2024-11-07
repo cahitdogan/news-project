262bc3ec53f251ddd59c55bdf9248cf928184989
Cahit Doğan <cahitdogan999@gmail.com>
2024-11-07 09:52:25 +0300

    DRY for MainSection

--- src/MainSection.tsx
+++ src/MainSection.tsx
@@ -1,42 +1,14 @@
-import test from "./images/test.png"
+import Articles from "./Articles";
 
 export default function MainSection() {
-    return(
+    return (
         <main>
-            <h2 
-                className="text-center text-2xl font-bold mb-5">
-                En Son Haberler</h2>
-            <section
-                className="grid grid-cols-1 gap-8 px-3 sm:grid-cols-2 lg:grid-cols-3">
-                    <article
-                        className="bg-slate-100 rounded-xl p-5">
-                            <img src={test} alt="..." className="m-auto"/>
-                            <h3 className="font-bold">Başlık</h3>
-                            <p className="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rerum, animi a eligendi dolore quibusdam?</p>
-                            <time className="text-sm" dateTime="2024-11-05">05.11.2024</time>
-                    </article>
-                    <article
-                        className="bg-slate-100 rounded-xl p-5">
-                            <img src={test} alt="..." className="m-auto"/>
-                            <h3 className="font-bold">Başlık</h3>
-                            <p className="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rerum, animi a eligendi dolore quibusdam?</p>
-                            <time className="text-sm" dateTime="2024-11-05">05.11.2024</time>
-                    </article>
-                    <article
-                        className="bg-slate-100 rounded-xl p-5">
-                            <img src={test} alt="..." className="m-auto"/>
-                            <h3 className="font-bold">Başlık</h3>
-                            <p className="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rerum, animi a eligendi dolore quibusdam?</p>
-                            <time className="text-sm" dateTime="2024-11-05">05.11.2024</time>
-                    </article>
-                    <article
-                        className="bg-slate-100 rounded-xl p-5">
-                            <img src={test} alt="..." className="m-auto"/>
-                            <h3 className="font-bold">Başlık</h3>
-                            <p className="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rerum, animi a eligendi dolore quibusdam?</p>
-                            <time className="text-sm" dateTime="2024-11-05">05.11.2024</time>
-                    </article>
-            </section>
+            <Articles title="World" category="general"/>
+            <Articles title="Business" category="business"/>
+            <Articles title="Technology" category="technology"/>
+            <Articles title="Sports" category="sports"/>
+            <Articles title="Science" category="science"/>
+            <Articles title="Entertainment" category="entertainment"/>
         </main>
     );
 }
