0b3cb7b70ec2e1d70449d9e9905ccdcb0c40c706
Cahit Doğan <cahitdogan999@gmail.com>
2024-11-07 11:18:47 +0300

    css changes

--- src/HeroSection.tsx
+++ src/HeroSection.tsx
@@ -3,7 +3,7 @@ import Currency from "./Currency.tsx";
 
 export default function HeroSection() {
     return(
-        <section className="m-auto w-11/12 justify-center md:w-4/5 lg:w-full lg:flex lg:gap-6 lg:px-5">
+        <section className="mb-10 m-auto w-full md:w-4/5 lg:w-full lg:flex lg:justify-center lg:gap-6 lg:px-5">
             <Carousel/>
             <Currency/>
         </section>
